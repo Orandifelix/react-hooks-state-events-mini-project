732049f45059e1ef1f9eaae2c14a9201ba909660
Orandifelix <125347734+Orandifelix@users.noreply.github.com>
2023-05-26 21:58:28 +0300

    Finished

--- src/components/CategoryFilter.js
+++ src/components/CategoryFilter.js
@@ -1,10 +1,20 @@
 import React from "react";
 
-function CategoryFilter() {
+function CategoryFilter({ categories, selectCategory, categoryName }) {
+  const categoryButtons = categories.map((category) => {
+    const categoryClass = category === selectCategory ? "selected" : "";
+
+    return (
+      <button key={category} onClick={categoryName} className={categoryClass}>
+        {category}
+      </button>
+    );
+  });
+
   return (
     <div className="categories">
       <h5>Category filters</h5>
-      {/* render <button> elements for each category here */}
+      {categoryButtons}
     </div>
   );
 }
--- src/components/TaskList.js
+++ src/components/TaskList.js
@@ -1,11 +1,27 @@
 import React from "react";
+import Task from "./Task";
 
-function TaskList() {
+function TaskList({ tasks, handleDelete }) {
+  
   return (
     <div className="tasks">
-      {/* display a list of tasks using Task component */}
+      {/* display a list of tasks */}
+
+      <ul>
+        {tasks.map((task) => (
+          <Task
+            key={task.text}
+            text={task.text}
+            category={task.category}
+            handleDelete={handleDelete}
+          />
+        ))}
+      </ul>
     </div>
   );
 }
 
 export default TaskList;
+
+
+
